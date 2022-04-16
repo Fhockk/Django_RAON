de7b9e85475540c55733bc33f1f24fe7f24d2f6c
Anatoliy Fhockk <anatoliy.fhock@gmail.com>
2022-04-16 13:54:35 +0300

    logo + local changes

--- shop/urls.py
+++ shop/urls.py
@@ -4,11 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('add_product/', views.create_product, name='add_product'),
-    path('<id>/', views.product_detail, name='product_detail'),
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
-    path('<category_slug>/', views.product_list, name='product_list_by_category'),
+    path('<int:id>/', views.product_detail, name='product_detail'),
+    path('<str:category_slug>/', views.product_list, name='product_list_by_category'),
+
 
 
 ]
